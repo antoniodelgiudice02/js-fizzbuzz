8b6d81da61cecf04e6b54352fffacffcd6c21ed4
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-12 01:31:07 +0100

    add: BUzz

--- JS/main.js
+++ JS/main.js
@@ -4,25 +4,38 @@ const row = document.getElementById('row')
 for(let i=1; i<=100; i++){
     console.log(i)
 
-    let fizz='';
+    let text='';
 
     if(i % 3 == 0){
-        fizz='fizz';
+        text='Fizz';
         row.innerHTML+=`
 
         <div class="col-3 g-3">
             <div class="card p-3 text-center">
-                <h3>${fizz}</h3>
+                <h3>${text}</h3>
             </div>
         </div>
     
     `
-    } else {
+    } else if(i % 5 == 0){
+        text='Buzz';
         row.innerHTML+=`
 
         <div class="col-3 g-3">
             <div class="card p-3 text-center">
-                <h3>${i} ${fizz}</h3>
+                <h3>${text}</h3>
+            </div>
+        </div>
+    
+    `
+    }
+    
+    else {
+        row.innerHTML+=`
+
+        <div class="col-3 g-3">
+            <div class="card p-3 text-center">
+                <h3>${i}</h3>
             </div>
         </div>
     
