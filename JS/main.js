6fb7e851087d3c74e43eea9554ce498fd2f3634b
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-12 11:03:00 +0100

    ottimizzazione JS

--- JS/main.js
+++ JS/main.js
@@ -4,57 +4,36 @@ const row = document.getElementById('row')
 for(let i=1; i<=100; i++){
     console.log(i)
 
-    let text='';
+    let text= '';
 
     if(i % 15 == 0){
 
         text='FizzBuzz';
-        row.innerHTML+=`
-
-        <div class="col-3 g-3">
-            <div class="card p-3 text-center">
-                <h3>${text}</h3>
-            </div>
-        </div>
-    
-    `
 
     } else if(i % 3 == 0){
 
         text='Fizz';
-        row.innerHTML+=`
 
-        <div class="col-3 g-3">
-            <div class="card p-3 text-center">
-                <h3>${text}</h3>
-            </div>
-        </div>
-    
-    `
     } else if(i % 5 == 0){
 
         text='Buzz';
-        row.innerHTML+=`
-
-        <div class="col-3 g-3">
-            <div class="card p-3 text-center">
-                <h3>${text}</h3>
-            </div>
-        </div>
     
-    `
     } else {
-        
-        row.innerHTML+=`
-
-        <div class="col-3 g-3">
-            <div class="card p-3 text-center">
-                <h3>${i}</h3>
-            </div>
-        </div>
     
-    `
+    text=i;
+
     }
+
+    
+    row.innerHTML+=`
+
+    <div class="col-3 g-3">
+        <div class="card p-3 text-center">
+            <h3>${text}</h3>
+        </div>
+    </div>
+
+`
 
      
     
