e2b2a9e1520038d9e4b0df150d5c7a1815b4c43f
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-12 01:27:27 +0100

    add: fizz

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,38 @@
+const row = document.getElementById('row')
+
 //Genero numeri da 1 a 100
 for(let i=1; i<=100; i++){
     console.log(i)
+
+    let fizz='';
+
+    if(i % 3 == 0){
+        fizz='fizz';
+        row.innerHTML+=`
+
+        <div class="col-3 g-3">
+            <div class="card p-3 text-center">
+                <h3>${fizz}</h3>
+            </div>
+        </div>
+    
+    `
+    } else {
+        row.innerHTML+=`
+
+        <div class="col-3 g-3">
+            <div class="card p-3 text-center">
+                <h3>${i} ${fizz}</h3>
+            </div>
+        </div>
+    
+    `
+
+    }
+    
+
+
+
+
 }
 
